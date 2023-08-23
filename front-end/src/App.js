e678d10fb8bebd6e211b7fc56b45ec0acc74d5f6
Vlad Ciobotariu <vladciobotariu@gmail.com>
2023-08-23 16:33:43 +0300

    added frontend

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "@fontsource/roboto";
+import Header from "components/Header";
+
+function App() {
+  return (
+      <div>
+        <Header/>
+      </div>
+  );
+}
+
+export default App;
--- src/main/java/com/ozius/internship/project/ProjectApplication.java
+++ src/main/java/com/ozius/internship/project/ProjectApplication.java
@@ -2,12 +2,16 @@ package com.ozius.internship.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class  ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
+
