19deba2fbf9d5e46587d47ceec300506f9863540
Vlad Ciobotariu <vladciobotariu@gmail.com>
2023-08-31 11:27:35 +0300

    code analysis

--- client-frontend/src/security/AuthContext.js
+++ client-frontend/src/security/AuthContext.js
@@ -1,4 +1,4 @@
-import {createContext, useContext, useEffect, useState} from "react";
+import {createContext, useContext} from "react";
 import { api } from "./ApiClient";
 import {executeJwtAuthenticationService, registerApiService} from "./AuthenticationApiService";
 import {useSessionStorage} from "../hooks/useSessionStorage";
