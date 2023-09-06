1cb65ea92eeaa79f21553904cff7fd54d60b3718
Vlad Ciobotariu <vladciobotariu@gmail.com>
2023-09-06 16:56:56 +0300

    small changes

--- src/test/java/com/ozius/internship/project/TestDataSeed.java
+++ src/test/java/com/ozius/internship/project/TestDataSeed.java
@@ -1,13 +1,9 @@
 package com.ozius.internship.project;
 
-import com.ozius.internship.project.entity.UserAccount;
-import com.ozius.internship.project.entity.buyer.Buyer;
 import com.ozius.internship.project.infra.JpaHelper;
 import jakarta.annotation.PostConstruct;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceUnit;
-import jakarta.transaction.Transactional;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
