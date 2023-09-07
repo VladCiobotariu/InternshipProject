4c00983d6e56f504a3c8933ba809d8744a9b835e
Erika Rusznak <erika.rusznak@gmail.com>
2023-09-07 08:53:25 +0300

    Do small cleanup

--- src/main/java/com/ozius/internship/project/infra/images/service/ImageService.java
+++ src/main/java/com/ozius/internship/project/infra/images/service/ImageService.java
@@ -1,9 +1,7 @@
 package com.ozius.internship.project.infra.images.service;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
