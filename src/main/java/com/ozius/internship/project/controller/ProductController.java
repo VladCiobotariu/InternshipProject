ef398bf2af9e59f809f8cf2c91842fbf5da51ffa
Erika Rusznak <erika.rusznak@gmail.com>
2023-09-11 19:22:34 +0300

    Add pagination for request

--- src/main/java/com/ozius/internship/project/controller/ProductController.java
+++ src/main/java/com/ozius/internship/project/controller/ProductController.java
@@ -1,21 +1,17 @@
 package com.ozius.internship.project.controller;
 
 import com.ozius.internship.project.dto.ProductDTO;
-import com.ozius.internship.project.entity.Product;
 import com.ozius.internship.project.repository.ProductRepository;
 import com.ozius.internship.project.service.ProductService;
-import com.ozius.internship.project.service.queries.ProductSearchQuery;
+import com.ozius.internship.project.service.queries.ProductPaginationSearchQuery;
 import jakarta.persistence.EntityManager;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
@@ -42,20 +38,21 @@ public class ProductController {
                                                                        @RequestParam(name = "priceFrom", required = false) Float priceFrom,
                                                                        @RequestParam(name = "priceTo", required = false) Float priceTo) {
 
+        String newCatName;
         // set categoryName first letter to uppercase
-        String newCatName = categoryName.substring(0, 1).toUpperCase() + categoryName.substring(1);
-        System.out.println(categoryName);
-        System.out.println(city);
-        ProductSearchQuery query = new ProductSearchQuery(modelMapper, entityManager)
-                .withCategory(newCatName);
-//                .withCity(city)
-//                .withPriceFrom(priceFrom)
-//                .withPriceTo(priceTo);
+        if(isNotEmpty(categoryName)) {
+            newCatName = categoryName.substring(0, 1).toUpperCase() + categoryName.substring(1);
+        } else {
+            newCatName = null;
+        }
+        ProductPaginationSearchQuery query = new ProductPaginationSearchQuery(modelMapper, entityManager)
+                .withCategory(newCatName)
+                .withCity(city)
+                .withPriceFrom(priceFrom)
+                .withPriceTo(priceTo);
 
-        Pageable pageable = PageRequest.of(page - 1, itemsPerPage);
 
-        List<ProductDTO> productsDTO = query.getResultList();
-
+        List<ProductDTO> productsDTO = query.getPagingResultList(itemsPerPage, page-1);
 
         return new ApiPaginationResponse<>(page, itemsPerPage, productsDTO.size(), productsDTO);
     }
---
+++ src/main/java/com/ozius/internship/project/service/queries/ProductPaginationSearchQuery.java
@@ -0,0 +1,41 @@
+package com.ozius.internship.project.service.queries;
+
+import com.ozius.internship.project.dto.ProductDTO;
+import com.ozius.internship.project.entity.Product;
+import jakarta.persistence.EntityManager;
+import org.modelmapper.ModelMapper;
+
+public class ProductPaginationSearchQuery extends PagingJpaQueryBuilder<Product,ProductDTO> {
+
+    private final ModelMapper modelMapper;
+
+    public ProductPaginationSearchQuery(ModelMapper modelMapper, EntityManager em) {
+        super("select p from Product p ",em, Product.class);
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public ResultTransformer<Product, ProductDTO> getTransformer() {
+        return new ModelMapperBasedResultTransformer<>(modelMapper, ProductDTO.class);
+    }
+
+    public ProductPaginationSearchQuery withCategory(String category){
+        and("p.category.name = :categoryName", "categoryName", category);
+        return this;
+    }
+
+    public ProductPaginationSearchQuery withCity(String city){
+        and("p.seller.legalAddress.city = :cityName", "cityName", city);
+        return this;
+    }
+
+    public ProductPaginationSearchQuery withPriceFrom(Float priceFrom){
+        and("p.price >= :priceFrom", "priceFrom", priceFrom);
+        return this;
+    }
+
+    public ProductPaginationSearchQuery withPriceTo(Float priceTo){
+        and("p.price <= :priceTo", "priceTo", priceTo);
+        return this;
+    }
+}
