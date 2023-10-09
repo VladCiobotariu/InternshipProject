c8d3112532e878024e23c815e82f1a3420489109
Vlad Ciobotariu <vladciobotariu@gmail.com>
2023-10-09 11:01:36 +0300

    changes

--- src/main/java/com/ozius/internship/project/service/listeners/ReviewAddedListener.java
+++ src/main/java/com/ozius/internship/project/service/listeners/ReviewAddedListener.java
@@ -3,8 +3,6 @@ package com.ozius.internship.project.service.listeners;
 import com.ozius.internship.project.entity.seller.ReviewAddedEvent;
 import com.ozius.internship.project.service.ProductService;
 import jakarta.transaction.Transactional;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -19,12 +17,12 @@ public class ReviewAddedListener {
     }
 
     /**
+     * @param event
      * Requires new transaction because current transaction resources are not yet closed but commited already. Any changes made would not be persisted.
      * See Warning message on TransactionalEventListener.
-     * @param event
      */
-   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-   @Transactional(value = Transactional.TxType.REQUIRES_NEW) //Requires new will always open a new transaction even if one already exists.
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(value = Transactional.TxType.REQUIRES_NEW) //Requires new will always open a new transaction even if one already exists.
     public void handleAddReview(ReviewAddedEvent event) {
         productService.recalculateProductRating(event.getProductId()); // the service will reuse the transaction opened by listener. (see default TxType = REQUIRED)
     }
