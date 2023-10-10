15d33dddcbb02d42cb5df62290a531cfd19c05e2
Erika Rusznak <erika.rusznak@gmail.com>
2023-10-10 18:29:40 +0300

    Delete unused exceptions

--- src/main/java/com/ozius/internship/project/entity/exception/IllegalDuplicateName.java
+++
@@ -1,7 +0,0 @@
-package com.ozius.internship.project.entity.exception;
-
-public class IllegalDuplicateName extends RuntimeException{
-    public IllegalDuplicateName(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/ozius/internship/project/entity/exception/IllegalQuantityException.java
+++
@@ -1,7 +0,0 @@
-package com.ozius.internship.project.entity.exception;
-
-public class IllegalQuantityException extends RuntimeException {
-    public IllegalQuantityException(String message) {
-        super(message);
-    }
-}
